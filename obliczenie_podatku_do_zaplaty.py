764f0695181d620cb993e79573344371bfcfc42a
Katarzyna Banaś <kb@MacBook-Pro-4.local>
2022-05-29 10:13:29 +0200

    1wszy commit funkcja

--- obliczenie_podatku_do_zaplaty.py
+++ obliczenie_podatku_do_zaplaty.py
@@ -1,2 +1,17 @@
-print("obliczenie_podatku_do_zaplaty")
-print("tresc")
+
+def obliczPodatek(zarobki):
+    podatekA = 10 / 100
+    podatekB = 20 / 100
+    podatekC = 30 / 100
+    if (zarobki < 2000):
+        podatek = podatekA;
+    elif (zarobki >= 2000 and zarobki < 5000):
+        podatek = podatekB;
+    else:
+        podatek = podatekC;
+    return podatek * zarobki
+
+
+# zarobki = int(input("Podaj wysokosc swoich zarobkow:"))
+# wysokoscPodatku = obliczPodatek(zarobki)
+# print("Wysokość Twojego podatku to:", wysokoscPodatku)
